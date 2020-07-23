a5548720649626dcf21309df2acab0a65eab27ed
Leoyang0717 <pithend58639@gmail.com>
2020-07-23 17:01:07 +0800

    WIP create board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,10 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+  def new
+  end
+  def create
+    Board.create(title: params[:title],intro: params[:intro])
+    redirect_to 'boards_path'
+  end
 end
