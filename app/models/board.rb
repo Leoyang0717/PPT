c002c74ea8986e330efa52f341146d9af05e8049
Leoyang0717 <pithend58639@gmail.com>
2020-07-30 12:23:00 +0800

    安裝paranoia 新增post_model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,15 @@
 class Board < ApplicationRecord
+  has_many :post
+
+  acts_as_paranoid
+
   validates :title ,:intro, presence: true
+
+  # def self.available
+  #   where(deleted_at: nil)
+  # end
+  # default_scope { where(deleted_at: nil) }
+  # scope :available, lambda { where(deleted_at: nil) }
 
   def destroy
     update(deleted_at: Time.now)
