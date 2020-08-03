a93241bf17b84d8575070768e60727547d85ebff
Leoyang0717 <pithend58639@gmail.com>
2020-08-03 15:40:25 +0800

    登出動作完成，驗證是否登入改寫在application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 class ApplicationController < ActionController::Base
+
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
-
-  before_action :find_user
+  # before_action :user_signed_in?,:current_user
+  helper_method :user_signed_in?,:current_user
 
   private
-  def find_user
-    @current_user = User.find(session[:user_token]) if session[:user_token]
-  end
-
   def not_found
     render file: '/public/404.html', status: 404
+  end
+  def user_signed_in?
+    current_user != nil
+  end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_token])
   end
 end
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,17 @@
 class BoardsController < ApplicationController
+  
+
   before_action :board_id,only: [:show,:edit,:update,:destroy]
   def index
     @boards = Board.all
   end
 
   def new
-    @board = Board.new
+    if user_signed_in?
+      @board = Board.new
+    else
+      redirect_to root_path,notice: "請先登入會員"
+    end
   end
 
   def create
